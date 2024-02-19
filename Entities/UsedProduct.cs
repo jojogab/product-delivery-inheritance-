91f3c006f5fd9ba37672a9dc730fcc8a3a371cff
Joa <jgfontes9@gmail.com>
2024-02-19 00:40:20 -0300

    UsedProduct class complet.

--- Entities/UsedProduct.cs
+++ Entities/UsedProduct.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Products.Entities
 {
-    internal class UsedProduct
+    internal class UsedProduct : Product
     {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct() 
+        { 
+        }
+
+        public UsedProduct(DateTime manufactureDate, string name, double price) : base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+
+        public override string priceTag()
+        {
+            string tag = $"{Name} (used) $ {Price} (Manufacture date: {ManufactureDate})";
+            return tag;
+        }
     }
 }
