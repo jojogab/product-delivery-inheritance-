0bd65867ef0109af6dd75bfed52bbf0dfebaf500
Joa <jgfontes9@gmail.com>
2024-02-19 00:45:27 -0300

    ImportedProduct class complet.

--- Entities/ImportedProduct.cs
+++ Entities/ImportedProduct.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Products.Entities
 {
-    internal class ImportedProduct
+    internal class ImportedProduct : Product
     {
+        public double CustomFee { get; set; }
+
+        public ImportedProduct() 
+        {
+        }
+
+        public ImportedProduct(double customFee, string name, double price) : base(name, price)
+        {
+            CustomFee = customFee;
+        }
+
+        public double totalPrice()
+        {
+            return Price + CustomFee;
+        }
+
+        public override string priceTag()
+        {
+            string tag = $"{Name} $ {totalPrice()} (Customs fee: $ {CustomFee})";
+            return tag;
+        }
     }
 }
